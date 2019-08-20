9e86d61e128c49e6fe3497bb1ae6da550974dd17
eleonore122 <40723433+eleonore122@users.noreply.github.com>
2019-08-20 01:04:00 -0500

    Add files via upload

--- thing/KinematicBody2D.gd
+++ thing/KinematicBody2D.gd
@@ -2,20 +2,14 @@ extends KinematicBody2D
 
 var speed = 200
 
-var velocity = Vector2()
+var Move_direction = Vector2(0,0)
 
-func get_input():
-        velocity = Vector2()
-        if Input.is_action_pressed('right'):
-            velocity.x += 1
-        if Input.is_action_pressed('left'):
-            velocity.x -= 1
-        if Input.is_action_pressed('down'):
-            velocity.y += 1
-        if Input.is_action_pressed('up'):
-            velocity.y -= 1
-        velocity = velocity.normalized() * speed
+func _ready():
+	pass
 
 func _physics_process(delta):
-    get_input()
-    velocity = move_and_slide(velocity)
+	Move_direction.x = -Input.get_action_strength('left') + Input.get_action_strength('right')
+	Move_direction.y = +Input.get_action_strength('down') - Input.get_action_strength('up')
+	
+	var velocity = Move_direction.normalized() * speed
+	velocity = move_and_slide(velocity)
