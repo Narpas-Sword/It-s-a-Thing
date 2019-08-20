c3c17c0329e683b8c46e5a9aef6ebe5516d85fcc
Narpas <cfbneutralist@gmail.com>
2019-08-20 01:52:39 -0600

    Code cleanup

--- thing/KinematicBody2D.gd
+++ thing/KinematicBody2D.gd
@@ -1,15 +1,18 @@
 extends KinematicBody2D
 
+const TICKRATE: int = 60
+
 # The player's movement speed in units per second. Set this instead of speed
-var movespeed = 200
-var speed = movespeed*60 # Multiplies the speed per second by 60 frames per second
+var movespeed: float = 200
+var speed: float = movespeed*TICKRATE # Multiplies the speed per second by the tickrate
+
 
 # Value between 0 and 1. Newton's first law; determines how fast the player's velocity changes. 0 means instantly, 1 means never
-var inertia = 0.8
+var inertia: float = 0.75
 
 # Vectors for us to do math on
-var velocity = Vector2(0, 0)
-var move_direction = Vector2(0,0)
+var velocity: Vector2 = Vector2(0, 0)
+var move_direction: Vector2 = Vector2(0,0)
 
 func _ready():
 	pass
