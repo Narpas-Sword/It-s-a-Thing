111e6ad9701f1f3c23c8589c7962a237945c9d10
Narpas <cfbneutralist@gmail.com>
2019-08-28 00:40:55 -0600

    Fixed export
    Also minor code cleanup

--- thing/scripts/KinematicBody2D.gd
+++ thing/scripts/KinematicBody2D.gd
@@ -1,12 +1,12 @@
 extends KinematicBody2D
 
 # The player's movement speed in units per second. Set this instead of speed
-export var movespeed: float = 200
-var speed: float = movespeed * options.TICKRATE # Multiplies the speed per second by the tickrate
+export(float) var movespeed = 200
+var _speed: float # This is the value we'll use for math, as it'll be multiplied by the tickrate
 
 
 # Value between 0 and 1. Newton's first law; determines how fast the player's velocity changes. 0 means instantly, 1 means never
-export(float, 1) var inertia: float = 0.75
+export(float, 1) var inertia = 0.75
 
 # Vectors for us to do math on
 var velocity: Vector2 = Vector2(0, 0)
@@ -16,7 +16,7 @@ func get_self():
 	return self.get_self()
 
 func _ready():
-	pass
+	_speed = movespeed * options.TICKRATE # Multiplies the speed per second by the tickrate
 
 func _physics_process(delta):
 	move_direction.x = -Input.get_action_strength('left') + Input.get_action_strength('right')
@@ -30,5 +30,5 @@ func _physics_process(delta):
 		$Sprite.scale.x = -4
 		$Sprite.position.x = -16
 	
-	velocity = velocity.linear_interpolate(move_direction.normalized() * speed * delta, 1 - inertia)
+	velocity = velocity.linear_interpolate(move_direction.normalized() * _speed * delta, 1 - inertia)
 	velocity = move_and_slide(velocity)
